e49957af0779ec79e84c3eeaba794fe7d9059a62
gelatus <40252423+gelatus@users.noreply.github.com>
2026-07-11 13:08:16 -0600

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int idx = -1;
+        int l=0, r = nums.length-1;
+
+        while (l <= r) {
+            //int mid = (l+r)/2;
+            int mid = l + (r - l) / 2;
+            if (target == nums[mid]) {
+                idx = mid;
+                break;
+            } else if (target < nums[mid]) {
+                r = mid - 1 ;
+            } else if (target > nums[mid]) {
+                l = mid + 1;
+            } 
+        }
+
+        return idx;
+
+    }
+}
